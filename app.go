272c15fe862e02b36d2642b4b8487beccd50449d
Max Lielje <max@maxlielje.co>
2015-07-12 16:04:49 +0200

    feat: connect clients immediately and authorize via message exchange
    Highly WIP, of course.
    
    The clients used to obtain a JWT through other means (HTTP post) to then
    connect on a websocket. Now everybody is connected instantly and the
    login information has to be sent through the socket.
    
    Start the app with `go run app.go`, visit localhost:5000, and type
    
    socket.emit("login", "password")
    
    then visit localhost:5000/connections to see the authorized connection

--- app.go
+++ app.go
@@ -13,13 +13,16 @@ import (
 )
 
 var (
-	connections  = make(map[string]socketio.Socket)
-	socketServer *socketio.Server
+	connections = make(map[string]socketio.Socket) // socket ID -> socket
+	sessions    = make(map[socketio.Socket]string) // socket -> session
+
+	socketServer *socketio.Server // main connection manager
 )
 
 var signingKey = []byte("dfb943aa439f")
 
 func init() {
+	// start a new socket server and make it available globally
 	s, err := socketio.NewServer(nil)
 	if err != nil {
 		log.Fatal(err)
@@ -29,61 +32,80 @@ func init() {
 }
 
 func main() {
-	socketServer.SetAllowRequest(func(req *http.Request) error {
-		req.ParseForm()
-		var userToken = req.Form.Get("token")
+	// socketServer.SetAllowRequest(func(req *http.Request) error {
+	// 	req.ParseForm()
+	// 	var userToken = req.Form.Get("token")
 
-		// dump, err := httputil.DumpRequest(req, true)
-		// log.Println(string(dump), err)
+	// 	// dump, err := httputil.DumpRequest(req, true)
+	// 	// log.Println(string(dump), err)
 
-		log.Println("jwt.Parse")
-		token, err := jwt.Parse(
-			userToken,
-			func(token *jwt.Token) (interface{}, error) {
-				// Don't forget to validate the alg is what you expect:
-				if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-					log.Println("Unexpected signing method", token.Header["alg"])
-					return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
-				}
-				log.Println("found key", string(signingKey))
-				return signingKey, nil
-			})
+	// 	// log.Println("jwt.Parse")
+	// 	token, err := jwt.Parse(
+	// 		userToken,
+	// 		func(token *jwt.Token) (interface{}, error) {
+	// 			// Don't forget to validate the alg is what you expect:
+	// 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+	// 				log.Println("Unexpected signing method", token.Header["alg"])
+	// 				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+	// 			}
+	// 			// log.Println("found key", string(signingKey))
+	// 			return signingKey, nil
+	// 		})
 
-		log.Println("done jwt.Parse")
+	// 	// log.Println("done jwt.Parse")
 
-		if err == nil && token.Valid {
-			return nil
-		}
+	// 	if err == nil && token.Valid {
+	// 		return nil
+	// 	}
+	// 	// log.Println("Invalid request")
 
-		return err
+	// 	return err
 
-		// return errors.New("Invalid token")
-	})
+	// 	// return errors.New("Invalid token")
+	// })
 
+	// called every time a client connects
 	socketServer.On("connection", func(so socketio.Socket) {
-		log.Println("connected", so.Id())
+		log.Println("[*]", so.Id())
 
-		so.Join("all")
+		// store the connection ID with the socket
 		connections[so.Id()] = so
 
-		so.On("dataRequest", dataRequest)
+		// everybody is part of 'all' and anonymous at first
+		so.Join("all")
+		so.Join("anon")
 
+		// register handlers for different types of messages that clients can send
+		so.On("dataRequest", dataRequest)
+		so.On("login", loginWSHandler)
+
+		// when a client disconnects remove the socket from active sessions
+		// not sure how to handle clients that disconnect briefly; probably the
+		// session shouldn’t be terminated but on the other hand the connection
+		// is gone and a new one needs to be established.
+		// This is probably the clients job by using the token to connect the socket
+		// and then the server only needs to check it on new connections to not parse
+		// the jwt for every message (or sth like that)
 		so.On("disconnection", func() {
-			log.Println("disconnected", so.Id())
-			delete(connections, so.Id())
+			log.Println("[x]", so.Id())
+			delete(connections, so.Id()) // remove from connection pool
+			delete(sessions, so)         // destroy active session, mainly for cleanup
 		})
 
 	})
 
+	// a general socket error occured
 	socketServer.On("error", func(so socketio.Socket, err error) {
 		log.Println("error:", err)
 	})
 
-	http.Handle("/socket.io/", socketServer)
+	// serve the static html
 	http.Handle("/", http.FileServer(http.Dir("./")))
-	http.HandleFunc("/send", sendHandler)
-	http.HandleFunc("/auth", authHandler)
 
+	// serve socket.io at the default path
+	http.Handle("/socket.io/", socketServer)
+
+	http.HandleFunc("/send", sendHandler)
 	http.HandleFunc("/connections", connectionsDebugHandler)
 
 	log.Println("Serving at localhost:5000...")
@@ -91,51 +113,71 @@ func main() {
 }
 
 func dataRequest(socket socketio.Socket, msg string) {
-	// socket.Request().ParseForm()
-	// dump, err := httputil.DumpRequest(socket.Request(), true)
-	// log.Println(string(dump), err)
-	// fmt.Println(socket.Request().Form)
-	log.Println("dataReq", socket.Id(), msg)
+	log.Printf("dataReq for connection %s: %q", socket.Id(), msg)
 }
 
-func authHandler(rw http.ResponseWriter, req *http.Request) {
-	req.ParseForm()
-	u := req.Form.Get("u")
-	p := req.Form.Get("p")
+// this will eventually be done through a GraphQL write
+func loginWSHandler(socket socketio.Socket, msg string) {
+	// validate credentials, just a string with a password for now
+	// this will be parsed from GraphQL later on the main graphql
+	// handler
 
-	if u == "admin" && p == "admin" {
+	// TODO: don’t do this again if socket is currently authenticated
+	log.Printf("Login request on socket %s with creds %q", socket.Id(), msg)
+	if msg == "password" {
+		log.Printf("Successfully authed conn %s for user with secret %s", socket.Id(), msg)
+
+		// generate JWT for client and save to active sessions
 		token := jwt.New(jwt.SigningMethodHS256)
 
 		// Set some claims
-		token.Claims["user"] = "admin"
 		token.Claims["userID"] = 1
 		token.Claims["exp"] = time.Now().Add(time.Hour * 72).Unix()
 
 		// Sign and get the complete encoded token as a string
-		tokenString, err := token.SignedString(signingKey)
+		tokenString, _ := token.SignedString(signingKey)
 
-		fmt.Fprintf(rw, "%s \t %s", tokenString, err)
-	}
-}
+		// save token to session store
+		sessions[socket] = tokenString
 
-func sendHandler(rw http.ResponseWriter, req *http.Request) {
-	req.ParseForm()
-	key := req.Form.Get("key")
+		// send token to client
+		socket.Emit("token", tokenString)
 
-	so, found := connections[key]
-	if found == false {
-		fmt.Fprintln(rw, "Session not found")
+		// user is no longer anonymous so we switch the room
+		socket.Leave("anon")
+		socket.Join("logged-in")
+
 		return
 	}
 
-	so.Emit("currentTime", time.Now().UnixNano())
-	socketServer.BroadcastTo("all", "hello", "Hi everyone!")
+	log.Println("Login attempt failed")
+}
 
-	fmt.Fprintln(rw, key)
+// broadcasts the current time to all clients
+func sendHandler(rw http.ResponseWriter, req *http.Request) {
+	// req.ParseForm()
+	// key := req.Form.Get("key")
+
+	// so, found := connections[key]
+	// if found == false {
+	// 	fmt.Fprintln(rw, "Socket ID not found")
+	// 	return
+	// }
+
+	msg := fmt.Sprintf("Broadcast! It is currently %s", time.Now())
+	socketServer.BroadcastTo("all", "currentTime", msg)
+
+	// fmt.Fprintln(rw, key)
+	fmt.Fprintln(rw, "Sent current time to all clients")
 }
 
 func connectionsDebugHandler(rw http.ResponseWriter, req *http.Request) {
-	for id, so := range connections {
-		fmt.Fprintf(rw, "%s\t%q\n", id, so.Rooms())
+	for socketID, socket := range connections {
+		var sessionToken string
+
+		if token, found := sessions[socket]; found {
+			sessionToken = token
+		}
+		fmt.Fprintf(rw, "%s\t%q\t%s\n", socketID, socket.Rooms(), sessionToken)
 	}
 }
